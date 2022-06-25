6dd29564cc80becafa79ec67b9c717dbbbf0c4a8
mirkomilablayev <mirkomilablayev777@gmail.com>
2022-06-25 09:42:18 +0500

    Updated

--- src/main/java/uz/crm/crmbackend/controller/controllers/HelloWorldController.java
+++ src/main/java/uz/crm/crmbackend/controller/controllers/HelloWorldController.java
@@ -1,13 +1,13 @@
-package uz.crm.crmbackend.controllers;
+package uz.crm.crmbackend.controller.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api/hello")
 @RequiredArgsConstructor
 public class HelloWorldController {
-    @GetMapping("/hello")
+    @GetMapping
     public String getHello(){
         return "Hello World";
     }

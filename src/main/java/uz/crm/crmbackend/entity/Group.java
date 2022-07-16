6ad0f40a945d7a9e566341dd3c5bf8cba7873d83
mirkomilablayev <mirkomilablayev777@gmail.com>
2022-07-16 16:50:18 +0500

    s

--- src/main/java/uz/crm/crmbackend/entity/Group.java
+++ src/main/java/uz/crm/crmbackend/entity/Group.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Entity
+@Entity(name = "groups")
 public class Group extends BaseEntityId implements BaseEntity {
     private String groupName;
     private LocalDateTime createdAt;
